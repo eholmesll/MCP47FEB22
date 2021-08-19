5155e7c52023639819764ec7dc2b2679278b4e46
Ed Holmes <eholmes@lablogic.com>
2021-08-19 11:07:43 +0000

    pointer changes

--- mcp47feb22.c
+++ mcp47feb22.c
@@ -1,51 +1,65 @@
 #include "mcp47feb22.h"
-#include "stm32l0xx.h"
-#include "stm32l0xx_hal_i2c.h"
-#include "stm32l0xx_hal.h"
-#include "stm32l0xx_hal_def.h"
-#include "stm32_adafruit_lcd.h"
+#include "stdio.h"
+#include "i2c.h"
+
 
 #define word(x,y) (((x) << 8) | (y))
 #define lowByte(x) ((x) & 0b00001111)
 
 
-static void _ReadEpAddr(DAC_TypeDef dac, uint8_t REG, unsigned char buffer[5]);
-static void _ReadAddr(DAC_TypeDef dac, uint8_t REG, unsigned char buffer[5]);
-static void _FastWrite(DAC_TypeDef dac, uint8_t REG, uint16_t DATA);
-static void _WriteAddr(DAC_TypeDef dac, uint8_t REG, uint8_t data);
+static unsigned char 	readBuffer[5];
+static unsigned char _buffer[5];
+static uint8_t      _dev_address;
+static uint8_t      _deviceID;
+static uint8_t      _intVref[2];
+static uint8_t      _gain[2];
+static uint8_t      _powerDown[2];
+static uint16_t     _values[2];
+static uint16_t     _valuesEp[2];
+static uint8_t      _intVrefEp[2];
+static uint8_t      _gainEp[2];
+static uint8_t      _powerDownEp[2];
+static uint8_t      _wiperLock[2];
+static uint16_t     _vdd;
+
+
+static void _ReadEpAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, unsigned char buffer[5]);
+static void _ReadAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, unsigned char buffer[5]);
+static void _FastWrite(MCP47FEB22_TypeDef *dac, uint8_t REG, uint16_t DATA);
+static void _WriteAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, uint8_t data);
 
 //DEVICE ID DEFAULT = 0
-void MCP47FEB22_Init(DAC_TypeDef* dac, uint8_t deviceID, I2C_HandleTypeDef hi2c){//uint8_t deviceID = 0x00) {
+void MCP47FEB22_Init(MCP47FEB22_TypeDef *dac, uint8_t deviceID, I2C_HandleTypeDef hi2c){//uint8_t deviceID = 0x00) {
 	dac->deviceID = deviceID;
 	dac->devAddr = (BASE_ADDR | (dac->deviceID));
-    dac->vdd = defaultVDD;
-    dac->hi2c = hi2c;
+	dac->vdd = defaultVDD;
+	dac->hi2c = hi2c;
 }
 
-static void _ReadEpAddr(DAC_TypeDef dac, uint8_t REG, unsigned char buffer[5]) {
+static void _ReadEpAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, unsigned char buffer[5]) {
 	uint8_t readReg = 0x80 | (READ | REG);// << 3);
 	
-	HAL_I2C_Master_Transmit(&dac.hi2c, dac.devAddr<<1, &readReg, 1, HAL_MAX_DELAY);
-	HAL_I2C_Master_Receive(&dac.hi2c, dac.devAddr<<1, buffer, 2, HAL_MAX_DELAY);
+	HAL_I2C_Master_Transmit(&dac->hi2c, dac->devAddr<<1, &readReg, 1, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(&dac->hi2c, dac->devAddr<<1, buffer, 2, HAL_MAX_DELAY);
 }
 
-static void _ReadAddr(DAC_TypeDef dac, uint8_t REG, unsigned char buffer[5]) {
+static void _ReadAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, unsigned char buffer[5]) {
 	uint8_t readReg = (READ | REG);// << 3);
-	HAL_I2C_Master_Transmit(&dac.hi2c, dac.devAddr<<1, &readReg, 1, HAL_MAX_DELAY);
-	HAL_I2C_Master_Receive(&dac.hi2c, dac.devAddr<<1, buffer, 2, HAL_MAX_DELAY);
+	HAL_I2C_Master_Transmit(&dac->hi2c, dac->devAddr<<1, &readReg, 1, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(&dac->hi2c, dac->devAddr<<1, buffer, 2, HAL_MAX_DELAY);
 }
 
 
-static void _FastWrite(DAC_TypeDef dac, uint8_t REG, uint16_t DATA) {
+static void _FastWrite(MCP47FEB22_TypeDef *dac, uint8_t REG, uint16_t DATA) {
 	uint8_t payload[8];
 	payload[0] = REG | WRITE;
 	payload[1] = (DATA >> 8) & 0xFF;
 	payload[2] = (DATA & 0xFF);
-	HAL_I2C_Master_Transmit(&dac.hi2c, dac.devAddr<<1, (uint8_t*)&payload, 3, HAL_MAX_DELAY);
+	HAL_I2C_Master_Transmit(&dac->hi2c, dac->devAddr<<1, (uint8_t*)&payload, 3, HAL_MAX_DELAY);
 }
 
 
-static void _WriteAddr(DAC_TypeDef dac, uint8_t REG, uint8_t data) {
+static void _WriteAddr(MCP47FEB22_TypeDef *dac, uint8_t REG, uint8_t data) {
 	uint8_t payload[3];
 	payload[0] = REG | WRITE;
 	if (REG == GAIN_REG) {
@@ -55,64 +69,64 @@ static void _WriteAddr(DAC_TypeDef dac, uint8_t REG, uint8_t data) {
 		payload[1] = 0;
 		payload[2] = data;
 	}
-	HAL_I2C_Master_Transmit(&dac.hi2c, dac.devAddr<<1, (uint8_t*)&payload, 3, HAL_MAX_DELAY);
+	HAL_I2C_Master_Transmit(&dac->hi2c, dac->devAddr<<1, (uint8_t*)&payload, 3, HAL_MAX_DELAY);
 
 }
 
 
-void MCP47FEB22_UnlockSALCK(DAC_TypeDef dac) {
+void MCP47FEB22_UnlockSALCK(MCP47FEB22_TypeDef *dac) {
 	//SET HVC PIN LOW
 	_WriteAddr(dac, UNLOCK_SALCK, 0);
 	// SET HVC PIN LOW
 }
 
-void MCP47FEB22_LockSALCK(DAC_TypeDef dac, uint8_t addr) {
+void MCP47FEB22_LockSALCK(MCP47FEB22_TypeDef *dac, uint8_t addr) {
 	//SET HVC PIN HIGH
-	DAC_TypeDef BIAS_DAC = {
+	MCP47FEB22_TypeDef BIAS_DAC = {
 		addr,
 		BASE_ADDR | addr,
-		dac.vdd,
-		dac.hi2c,
+		dac->vdd,
+		dac->hi2c,
 	};
 
-	_WriteAddr(BIAS_DAC, LOCK_SALCK, 0);
+	_WriteAddr(&BIAS_DAC, LOCK_SALCK, 0);
 	//SET HVC PIN LOW
 }
 
-void MCP47FEB22_ChangeAddr(DAC_TypeDef dac, uint8_t addr) {
-	BSP_LCD_DisplayString((uint8_t*)"HVC ON... Unlocking SALCK", 0, 45, SMALL_FONT);
-	HAL_Delay(10000);
+void MCP47FEB22_ChangeAddr(MCP47FEB22_TypeDef *dac, uint8_t addr) {
+	//BSP_LCD_DisplayString((uint8_t*)"HVC ON... Unlocking SALCK", 0, 45, SMALL_FONT);
+	//HAL_Delay(10000);
 	
-	MCP47FEB22_UnlockSALCK(dac);
+	//MCP47FEB22_UnlockSALCK(dac);
 
-	BSP_LCD_DisplayString((uint8_t*)"HVC OFF...send addr change", 0, 60, SMALL_FONT);
-	HAL_Delay(10000);
+	//BSP_LCD_DisplayString((uint8_t*)"HVC OFF...send addr change", 0, 60, SMALL_FONT);
+	//HAL_Delay(10000);
 
-	_WriteAddr(dac, 0b11010000, BASE_ADDR|addr);
+	//_WriteAddr(dac, 0b11010000, BASE_ADDR|addr);
 
-	BSP_LCD_DisplayString((uint8_t*)"HVC ON... Locking SALCK", 0, 75, SMALL_FONT);
-	HAL_Delay(10000);
+	//BSP_LCD_DisplayString((uint8_t*)"HVC ON... Locking SALCK", 0, 75, SMALL_FONT);
+	//HAL_Delay(10000);
 
-	MCP47FEB22_LockSALCK(dac, addr);
+	//MCP47FEB22_LockSALCK(dac, addr);
 
-	BSP_LCD_DisplayString((uint8_t*)"HVC OFF...", 0, 90, SMALL_FONT);
-	HAL_Delay(10000);
+	//BSP_LCD_DisplayString((uint8_t*)"HVC OFF...", 0, 90, SMALL_FONT);
+	//HAL_Delay(10000);
 
 }
 
-uint8_t MCP47FEB22_GetPowerDown(DAC_TypeDef dac, int channel) {
+uint8_t MCP47FEB22_GetPowerDown(MCP47FEB22_TypeDef *dac, int channel) {
 	_ReadAddr(dac, PD_REG, readBuffer);
 	_powerDown[0] = (readBuffer[1] & 0B00000011);
 	_powerDown[1] = (readBuffer[1] & 0B00001100) >> 2;
 	return (channel == 0) ? _powerDown[0] : _powerDown[1];
 }
 
-void MCP47FEB22_SetPowerDown(DAC_TypeDef dac, int val0, int val1) {
+void MCP47FEB22_SetPowerDown(MCP47FEB22_TypeDef *dac, int val0, int val1) {
 	_WriteAddr(dac, PD_REG, (val0 | val1<<2));
 }
 
 
-uint8_t MCP47FEB22_GetPowerDownEp(DAC_TypeDef dac, int channel) {
+uint8_t MCP47FEB22_GetPowerDownEp(MCP47FEB22_TypeDef *dac, int channel) {
 	_ReadEpAddr(dac, PD_REG, readBuffer);
 	_powerDownEp[0] = (readBuffer[1] & 0B00000011);
 	_powerDownEp[1] = (readBuffer[1] & 0B00001100) >> 2;
@@ -120,7 +134,7 @@ uint8_t MCP47FEB22_GetPowerDownEp(DAC_TypeDef dac, int channel) {
 }
 
 
-uint8_t MCP47FEB22_GetGain(DAC_TypeDef dac, int channel) {
+uint8_t MCP47FEB22_GetGain(MCP47FEB22_TypeDef *dac, int channel) {
 	uint8_t buff[5] = {0};
 	_ReadAddr(dac, GAIN_REG, buff);
 	_gain[0] = (buff[0] & 0B00000001);
@@ -129,55 +143,48 @@ uint8_t MCP47FEB22_GetGain(DAC_TypeDef dac, int channel) {
 }
 
 
-void MCP47FEB22_SetGain(DAC_TypeDef dac, int val0, int val1) {
+void MCP47FEB22_SetGain(MCP47FEB22_TypeDef *dac, int val0, int val1) {
 	_WriteAddr(dac, GAIN_REG, (val0 | (val1<<1)));
 }
 
-uint8_t MCP47FEB22_GetGainEp(DAC_TypeDef dac, int channel) {
+uint8_t MCP47FEB22_GetGainEp(MCP47FEB22_TypeDef *dac, int channel) {
 	_ReadEpAddr(dac, GAIN_REG, readBuffer);
 	_gainEp[0] = (readBuffer[0] & 0B00000001);
 	_gainEp[1] = (readBuffer[0] & 0B00000010)>>1;
 	return (channel == 0) ? _gainEp[0] : _gainEp[1];
 }
 
-
-
-
-uint8_t MCP47FEB22_GetVref(DAC_TypeDef dac, uint8_t channel) {//uint8_t channel) {
+uint8_t MCP47FEB22_GetVref(MCP47FEB22_TypeDef *dac, uint8_t channel) {//uint8_t channel) {
 	_ReadAddr(dac,VREF_REG, readBuffer);
 	_intVref[0] = (readBuffer[1] & 0b00000011);
 	_intVref[1] = (readBuffer[1] & 0b00001100) >> 2;
 	return (channel == 0) ? _intVref[0] : _intVref[1];
 }
 
-void MCP47FEB22_SetVref(DAC_TypeDef dac, uint8_t val0, uint8_t val1) {
+void MCP47FEB22_SetVref(MCP47FEB22_TypeDef *dac, uint8_t val0, uint8_t val1) {
 	_WriteAddr(dac, VREF_REG, (val0 | (val1<<2)));
 }
 
-uint8_t MCP47FEB22_GetVrefEp(DAC_TypeDef dac, uint8_t channel) {//uint8_t channel) {
+uint8_t MCP47FEB22_GetVrefEp(MCP47FEB22_TypeDef *dac, uint8_t channel) {//uint8_t channel) {
 	_ReadEpAddr(dac, VREF_REG, readBuffer);
 	_intVrefEp[0] = (readBuffer[1] & 0b00000011);
 	_intVrefEp[1] = (readBuffer[1] & 0b00001100) >> 2;
 	return (channel == 0) ? _intVrefEp[0] : _intVrefEp[1];
 }
 
-
-
-
-
-uint16_t MCP47FEB22_GetValue(DAC_TypeDef dac, uint8_t channel) {
+uint16_t MCP47FEB22_GetValue(MCP47FEB22_TypeDef *dac, uint8_t channel) {
 	_ReadAddr(dac, (channel << 3), readBuffer);
 	return word((readBuffer[0] & 0B00001111), readBuffer[1]);
 }
 
-void MCP47FEB22_AnalogWrite(DAC_TypeDef dac, uint16_t val0, uint16_t val1) {
+void MCP47FEB22_AnalogWrite(MCP47FEB22_TypeDef *dac, uint16_t val0, uint16_t val1) {
 	val0 &= 0xFFF;
 	val1 &= 0xFFF; //Prevent going over 4095
 	_FastWrite(dac, DAC0_REG, val0);
 	_FastWrite(dac, DAC1_REG, val1);
 }
 
-void MCP47FEB22_EEPROMWrite(DAC_TypeDef dac) {
+void MCP47FEB22_EEPROMWrite(MCP47FEB22_TypeDef *dac) {
 	_FastWrite(dac, DAC0_EP_REG, MCP47FEB22_GetValue(dac, 0));
 	_FastWrite(dac, DAC1_EP_REG, MCP47FEB22_GetValue(dac, 1));
 
--- mcp47feb22.h
+++ mcp47feb22.h
@@ -1,7 +1,6 @@
 #ifndef __MCP47FEB22_H
 #define __MCP47FEB22_H
 
-#include "stm32l0xx.h"
 #include "i2c.h"
 
 // REG ADDRESSES ALREADY << 3
@@ -29,48 +28,33 @@
 #define LOCK_SALCK 0b11010100
 
 
-unsigned char 	readBuffer[5];
 
-unsigned char _buffer[5];
-uint8_t      _dev_address;
-uint8_t      _deviceID;
-uint8_t      _intVref[2];
-uint8_t      _gain[2];
-uint8_t      _powerDown[2];
-uint16_t     _values[2];
-uint16_t     _valuesEp[2];
-uint8_t      _intVrefEp[2];
-uint8_t      _gainEp[2];
-uint8_t      _powerDownEp[2];
-uint8_t      _wiperLock[2];
-uint16_t     _vdd;
-
- typedef struct {
+typedef struct {
 	uint8_t deviceID;
 	uint8_t devAddr;
 	uint16_t vdd;
 	I2C_HandleTypeDef hi2c;
-} DAC_TypeDef;
+} MCP47FEB22_TypeDef;
 
 
 
 
-void MCP47FEB22_Init(DAC_TypeDef* dac, uint8_t deviceID, I2C_HandleTypeDef hi2c);
-void  MCP47FEB22_UnlockSALCK(DAC_TypeDef dac);
-void  MCP47FEB22_LockSALCK(DAC_TypeDef dac, uint8_t addr);
-void MCP47FEB22_ChangeAddr(DAC_TypeDef dac, uint8_t addr);
-uint8_t MCP47FEB22_GetPowerDown(DAC_TypeDef dac, int channel);
-void MCP47FEB22_SetPowerDown(DAC_TypeDef dac, int val0, int val1);
-uint8_t MCP47FEB22_GetPowerDownEp(DAC_TypeDef dac, int channel);
-uint8_t MCP47FEB22_GetGain(DAC_TypeDef dac, int channel);
-void MCP47FEB22_SetGain(DAC_TypeDef dac, int val0, int val1);
-uint8_t MCP47FEB22_GetGainEp(DAC_TypeDef dac, int channel);
-uint8_t MCP47FEB22_GetVref(DAC_TypeDef dac, uint8_t channel);
-void MCP47FEB22_SetVref(DAC_TypeDef dac, uint8_t val0, uint8_t val1);
-uint8_t MCP47FEB22_GetVrefEp(DAC_TypeDef dac, uint8_t channel);
-uint16_t MCP47FEB22_GetValue(DAC_TypeDef dac, uint8_t channel);
-void MCP47FEB22_AnalogWrite(DAC_TypeDef dac, uint16_t val0, uint16_t val1);
-void MCP47FEB22_EEPROMWrite(DAC_TypeDef dac);
+void MCP47FEB22_Init(MCP47FEB22_TypeDef *dac, uint8_t deviceID, I2C_HandleTypeDef hi2c);
+void  MCP47FEB22_UnlockSALCK(MCP47FEB22_TypeDef *dac);
+void  MCP47FEB22_LockSALCK(MCP47FEB22_TypeDef *dac, uint8_t addr);
+void MCP47FEB22_ChangeAddr(MCP47FEB22_TypeDef *dac, uint8_t addr);
+uint8_t MCP47FEB22_GetPowerDown(MCP47FEB22_TypeDef *dac, int channel);
+void MCP47FEB22_SetPowerDown(MCP47FEB22_TypeDef *dac, int val0, int val1);
+uint8_t MCP47FEB22_GetPowerDownEp(MCP47FEB22_TypeDef *dac, int channel);
+uint8_t MCP47FEB22_GetGain(MCP47FEB22_TypeDef *dac, int channel);
+void MCP47FEB22_SetGain(MCP47FEB22_TypeDef *dac, int val0, int val1);
+uint8_t MCP47FEB22_GetGainEp(MCP47FEB22_TypeDef *dac, int channel);
+uint8_t MCP47FEB22_GetVref(MCP47FEB22_TypeDef *dac, uint8_t channel);
+void MCP47FEB22_SetVref(MCP47FEB22_TypeDef *dac, uint8_t val0, uint8_t val1);
+uint8_t MCP47FEB22_GetVrefEp(MCP47FEB22_TypeDef *dac, uint8_t channel);
+uint16_t MCP47FEB22_GetValue(MCP47FEB22_TypeDef *dac, uint8_t channel);
+void MCP47FEB22_AnalogWrite(MCP47FEB22_TypeDef *dac, uint16_t val0, uint16_t val1);
+void MCP47FEB22_EEPROMWrite(MCP47FEB22_TypeDef *dac);
 
 
 
